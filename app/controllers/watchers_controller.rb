d47914a07cdba1f3eac04724c4ab9ad9f8342ab1
Steve McKinney <steve.mckinney@gmail.com>
2014-10-18 16:56:14 -0400

    Add pretty urls to Watcher

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -64,7 +64,7 @@ class WatchersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_watcher
-      @watcher = Watcher.find(params[:id])
+      @watcher = Watcher.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/watcher.rb
+++ app/models/watcher.rb
@@ -1,3 +1,6 @@
 class Watcher < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, :use => :slugged
+
   has_many :tweets
 end
