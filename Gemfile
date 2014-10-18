04edd561e010d086574dfcdcf354437c9c126705
Steve McKinney <steve.mckinney@gmail.com>
2014-10-18 15:11:52 -0400

    Add Tweet model
    * with proof of concept rake task
    * show tweets on watcher page

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'rails_12factor', group: :production
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'twitter'
+
 # Use unicorn as the app server
 gem 'unicorn'
 
---
+++ app/models/tweet.rb
@@ -0,0 +1,6 @@
+class Tweet < ActiveRecord::Base
+  belongs_to :watcher
+  store_accessor :fields, :favorite_count, :filter_level, :in_reply_to_screen_name,
+                          :in_reply_to_attrs_id, :in_reply_to_status_id, :in_reply_to_user_id,
+                          :lang, :retweet_count, :source, :text
+end
