445e7f797c7856def5f45116e4ee64aeeb41e4a7
Steve McKinney <steve.mckinney@gmail.com>
2014-10-18 18:25:12 -0400

    Add more attr accessors

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ActiveRecord::Base
   belongs_to :watcher
-  store_accessor :fields, :favorite_count, :filter_level, :in_reply_to_screen_name,
+  store_accessor :fields, :entities, :favorite_count, :filter_level, :in_reply_to_screen_name,
                           :in_reply_to_attrs_id, :in_reply_to_status_id, :in_reply_to_user_id,
-                          :lang, :retweet_count, :source, :text
+                          :lang, :retweet_count, :source, :text, :user
 end
