a6a70ba40aea7925a7e0cb56f943df4e68cfde06
Sean Dillingham <sean.dillingham@gmail.com>
2014-10-19 10:28:06 -0400

    refactor filters js

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function() {
   $(".facets .sort").click(function() {
     $(".facets .sort").removeClass("active");
     $(this).addClass("active");
+    if ($(this).hasClass("recent") == true) {
+      $(".tweet-container").tsort('',{order:'asc',attr:'time'});
+    }
+    else if ($(this).hasClass("retweets") == true) {
+      $(".tweet-container").tsort('',{order:'asc',attr:'retweets'});
+    }
+    else if ($(this).hasClass("media") == true) {
+      $(".tweet-container").tsort('',{order:'asc',attr:'has-media'});
+    }
     return false;
-    if ($(this).attr("class") == "recent") {
-      $(".tweets").tsort('.tweet-container',{order:'asc',attr:'time'});
-    }
-    else if ($(this).attr("class") == "retweets") {
-      $(".tweets").tsort('.tweet-container',{order:'asc',attr:'retweets'});
-    }
-    else if ($(this).attr("class") == "media") {
-      $(".tweets").tsort('.tweet-container',{order:'asc',attr:'media'});
-    }
   }); 
 });
