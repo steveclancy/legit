830c45d279c526cf070e615be20a0924f21e9bbb
Sean Dillingham <sean.dillingham@gmail.com>
2014-10-19 10:57:20 -0400

    more filter refacforing

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,17 +1,35 @@
 jQuery(document).ready(function() {
-  // Code here 
-  $(".facets .sort").click(function() {
+
+  $(".facets .sort.retweets").click(function() {
     $(".facets .sort").removeClass("active");
     $(this).addClass("active");
-    if ($(this).hasClass("recent") == true) {
-      $(".tweet-container").tsort('',{order:'asc',attr:'time'});
-    }
-    else if ($(this).hasClass("retweets") == true) {
-      $(".tweet-container").tsort('',{order:'asc',attr:'retweets'});
-    }
-    else if ($(this).hasClass("media") == true) {
-      $(".tweet-container").tsort('',{order:'asc',attr:'has-media'});
-    }
-    return false;
-  }); 
+    event.preventDefault();
+    $(".tweet-container").sort(function(a,b){
+        return new $(a).attr("data-retweets") > new $(b).attr("data-retweets");
+    }).each(function(){
+        $(".tweets").prepend(this);
+    });
+  });
+
+  $(".facets .sort.media").click(function() {
+    $(".facets .sort").removeClass("active");
+    $(this).addClass("active");
+    event.preventDefault();
+    $(".tweet-container").sort(function(a,b){
+        return new $(a).attr("data-has-media") > new $(b).attr("data-has-media");
+    }).each(function(){
+        $(".tweets").prepend(this);
+    });
+  });
+
+  $(".facets .sort.recent").click(function() {
+    $(".facets .sort").removeClass("active");
+    $(this).addClass("active");
+    event.preventDefault();
+    $(".tweet-container").sort(function(a,b){
+        return new $(a).attr("data-time") > new $(b).attr("data-time");
+    }).each(function(){
+        $(".tweets").prepend(this);
+    });
+  });
 });
