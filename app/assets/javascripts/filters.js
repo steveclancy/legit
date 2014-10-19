05351ed5f87e7544c51b9ca2bcd2e063edb52e9d
Steve Clancy <steve.clancy@gmail.com>
2014-10-19 10:53:21 -0400

    Filter commented out, stuff

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function() {
   $(".facets .sort").click(function() {
     $(".facets .sort").removeClass("active");
     $(this).addClass("active");
+    return false;
     if ($(this).attr("class") == "recent") {
       $(".tweets").tsort('.tweet-container',{order:'asc',attr:'time'});
     }
@@ -12,7 +13,11 @@ jQuery(document).ready(function() {
     else if ($(this).attr("class") == "media") {
       $(".tweets").tsort('.tweet-container',{order:'asc',attr:'media'});
     }
-    return false;
-    
+
+    $('.tweet-container').each(function(i, v) {
+      $this = $(v);
+
+      $(this).find('iframe').replaceWith($this.find('blockquote').html());
+    })
   }); 
 });
