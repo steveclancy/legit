f8fb617aab29f5366ce37d0272ba046766a3714d
Steve McKinney <steve.mckinney@gmail.com>
2014-10-19 10:11:55 -0400

    Add has_media and screen name to tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,15 @@ class Tweet < ActiveRecord::Base
   store_accessor :fields, :entities, :favorite_count, :filter_level, :in_reply_to_screen_name,
                           :in_reply_to_attrs_id, :in_reply_to_status_id, :in_reply_to_user_id,
                           :lang, :retweet_count, :source, :text, :user
+
+  def has_media?
+    e = eval(entities)
+    media = e.fetch(:media, nil)
+    media.present?
+  end
+
+  def screen_name
+    u = eval(user)
+    u.fetch(:screen_name, nil)
+  end
 end
